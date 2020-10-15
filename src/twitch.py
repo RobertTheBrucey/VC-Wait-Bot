9415d042ef741e664019b8c77ec52da3cd1a6e4b
Sean Ashton <sean@ashtech.one>
2020-10-16 00:17:46 +0800

    Troubleshooting

--- src/twitch.py
+++ src/twitch.py
@@ -65,7 +65,7 @@ class T_Bot(commands.Bot):
         chan = self.db.query(TwitchChannel).filter(TwitchChannel.name==ctx.channel.name).one_or_none()
         if ctx.author.is_mod or ctx.author.name == chan.name:
             print("Is mod")
-            print(chan.name)
+            print(chan)
             if not chan is None:
                 print("Verified")
                 chan.verified = True
