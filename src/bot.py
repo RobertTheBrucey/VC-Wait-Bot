1d74ba5f5990c395c2877571ea57836996d8ffd4
Sean Ashton <sean@ashtech.one>
2020-10-15 23:37:00 +0800

    Troubleshooting

--- src/bot.py
+++ src/bot.py
@@ -307,7 +307,7 @@ async def get_user(user_id, db=db): #Add use if not in db
 
 async def check_auth(ctx):
     allowed = False
-    owner = await bot.application_info().owner
+    owner = None#await bot.application_info().owner
     if ctx.message.author == owner:
         allowed = True
     elif ctx.message.author.guild_permissions.administrator:
