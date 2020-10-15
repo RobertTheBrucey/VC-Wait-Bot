f310c1da2075d0ea98d76d8cf8fe61ce273941ab
Sean Ashton <sean@ashtech.one>
2020-10-15 23:34:21 +0800

    Troubleshooting

--- src/bot.py
+++ src/bot.py
@@ -232,12 +232,15 @@ async def update_last(guild_d, db=db):
     users = guild.users
     msg = None
     for c in guild_d.text_channels:
-        msg = await c.fetch_message(guild.lastedit)
+        try:
+            msg = await c.fetch_message(guild.lastedit)
+        except:
+            pass
         if not msg is None:
             break
     if msg is None:
         return
-        
+
     tl = (msg.edited_at.timestamp() + guild.cooldown + 1) - int(time.time())
     if tl <= 0:
         if len(users) > 0:
